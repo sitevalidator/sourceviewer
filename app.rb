ff492050dfcff804bc7a83c814ac595d502b39c8
Jaime Iniesta <jaimeiniesta@gmail.com>
2013-11-18 23:22:10 +0100

    include a configurable user_agent, with a default

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'open_uri_redirections'
 
 get '/show' do
   @url    = params[:url]
-  @source = open(@url, allow_redirections: :all).read
+  @source = open(@url, allow_redirections: :all, "User-Agent" => user_agent).read
 
   erb :show
 end
@@ -13,4 +13,14 @@ helpers do
   def h(text)
     Rack::Utils.escape_html(text)
   end
+end
+
+private
+
+def user_agent
+  params[:user_agent] || default_user_agent
+end
+
+def default_user_agent
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36"
 end
