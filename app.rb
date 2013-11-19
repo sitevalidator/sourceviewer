e895066c7984163192d219ace768e1ae3e0c427b
Jaime Iniesta <jaimeiniesta@gmail.com>
2013-11-19 01:17:38 +0100

    personalized 500 error message

--- app.rb
+++ app.rb
@@ -2,11 +2,21 @@ require 'sinatra'
 require 'open-uri'
 require 'open_uri_redirections'
 
+set :show_exceptions, false
+
 get '/show' do
   @url    = params[:url]
   @source = open(@url, allow_redirections: :all, "User-Agent" => user_agent).read
 
   erb :show
+end
+
+get '/500' do
+  return "SORRY! We could not process this page."
+end
+
+error 500 do
+  redirect to('/500')
 end
 
 helpers do
